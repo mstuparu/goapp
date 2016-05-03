576c33821ade4e676f37f74d984275407eccd86c
Marius Stuparu <mstuparu@rmn.com>
2016-05-03 07:02:39 +0100

    Bump version

--- src/app.go
+++ src/app.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) { 
         h, _ := os.Hostname()
-        fmt.Fprintf(w, "Hi there v5, I'm served from %s!", h) 
+        fmt.Fprintf(w, "Hi there v7, I'm served from %s!", h) 
 } 
 
 func main() { 
